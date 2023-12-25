2deca2601e612a53326890165ffaed99e6d70b35
slavaPrus <slava.ei@gmail.com>
2023-12-25 08:43:58 +0200

    mark dto

---
+++ TiferetShlomo/Controllers/MarkController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TiferetShlomoBL;
+using TiferetShlomoDAL.Models;
+using TiferetShlomoDTO.DTO;
+
+namespace TiferetShlomo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MarkController : ControllerBase
+    {
+        private readonly IMarkBL _markBL;
+
+        public MarkController(IMarkBL markBL)
+        {
+            _markBL = markBL;
+        }
+
+        // GET: api/Mark
+        [HttpGet]
+        public IEnumerable<MarkDTO> GetAllMarks()
+        {
+            return _markBL.GetAllMarks();
+        }
+
+        // GET: api/Mark/5
+        [HttpGet("{id}")]
+        public ActionResult<Mark> GetMarkById(int id)
+        {
+            var mark = _markBL.GetMarkById(id);
+            if (mark == null)
+            {
+                return NotFound();
+            }
+            return mark;
+        }
+
+        // POST: api/Mark
+        [HttpPost]
+        public IActionResult AddMark(MarkDTO mark)
+        {
+            _markBL.AddMark(mark);
+            return CreatedAtAction(nameof(GetMarkById), new { id = mark.MarkId }, mark);
+        }
+
+        // PUT: api/Mark/5
+        [HttpPut("{id}")]
+        public IActionResult UpdateMark(int id, Mark mark)
+        {
+            if (id != mark.MarkId)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                _markBL.UpdateMark(mark);
+            }
+            catch (Exception)
+            {
+                if (_markBL.GetMarkById(id) == null)
+                {
+                    return NotFound();
+                }
+                throw;
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Mark/5
+        [HttpDelete("{id}")]
+        public IActionResult DeleteMark(int id)
+        {
+            var existingMark = _markBL.GetMarkById(id);
+            if (existingMark == null)
+            {
+                return NotFound();
+            }
+
+            _markBL.RemoveMark(id);
+
+            return NoContent();
+        }
+    }
+}
+   
---
+++ TiferetShlomoBL/MarkBL.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TiferetShlomoDAL.Models;
+using TiferetShlomoDAL;
+using AutoMapper;
+using TiferetShlomoDTO.DTO;
+
+
+namespace TiferetShlomoBL
+{
+    public class MarkBL : IMarkBL
+    {
+        private readonly IMarkDAL _markDAL;
+        private readonly IMapper _mapper;
+
+
+        public MarkBL(IMarkDAL markDAL, IMapper mapper)
+        {
+            _markDAL = markDAL;
+            _mapper = mapper;
+        }
+
+        public IEnumerable<MarkDTO> GetAllMarks()
+        {
+            IEnumerable<Mark> marks = _markDAL.GetAllMarks();
+
+            IEnumerable<MarkDTO> marksDTO = _mapper.Map<IEnumerable<MarkDTO>>(marks);
+
+            return marksDTO;
+
+        }
+
+        public Mark GetMarkById(int id)
+        {
+            return _markDAL.GetMarkById(id);
+        }
+
+        public void AddMark(MarkDTO mark)
+        {
+            // Perform business logic validations or additional operations if needed
+            Mark m = _mapper.Map<Mark>(mark);
+            _markDAL.AddMark(m);
+        }
+
+        public void UpdateMark(Mark mark)
+        {
+            // Perform business logic validations or additional operations if needed
+            _markDAL.UpdateMark(mark);
+        }
+
+        public void RemoveMark(int id)
+        {
+            // Perform business logic validations or additional operations if needed
+            _markDAL.RemoveMark(id);
+        }
+    }
+
+}
+
